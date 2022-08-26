fd85cc135e12aa37fa4c266d379a91fa7bdfc1e0
flopyalegris <floberriel23@gmail.com>
2022-08-26 18:26:55 -0300

    Entrega 2.1 y 2.2

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+//esta funcion hace que la variable USER declarado en localStorage sea false y como se ve mas abajo me redirecciona a login
+function desconectarse(){
+    localStorage.setItem('USER', 'false');
+}
+
+//esta funcion validarUsuario la cree para poder ver si el usuario pudo loguearse con exito e impedir que entre sin hacerlo
+
+//onload atributo
+function validarUsuario() {
+    if(localStorage.getItem('USER')){ //verifica si el USER existe
+    
+        if(localStorage.getItem('USER')  == 'false'){
+            alert('Debe logearse correctamente')
+            window.location.href = "login.html";//si el usuario es false (no cumplir con mi validacion) rebota a login
+    }
+    } else {
+    window.location.href = "login.html"; 
+    alert('Debe logearse correctamente'); //esto impide que el cliente navegue por el index sin pasar por el login previamente (y lo redirige)
+}
+document.getElementById("muestra-usuario").innerHTML = localStorage.getItem("usuario"); 
+}//ENTREGA2 linea 34 selecciona con el getElement el elemento con id "muestra-usuario" y con el inner muestra el localStorage en pantalla(html)
+
---
+++ js/login.js
@@ -0,0 +1,27 @@
+function alertaExitosa() {
+    alert("Inicio de Sesión exitoso");
+};
+
+function alertaDenegada() {
+    alert("Ups! Tuvimos un problema para ingresar");
+};
+//onclick atributo
+function validar() {
+    email = document.getElementById("mail");
+    password = document.getElementById("password");
+        if (email.value == 0 || password.value == 0) {
+            alertaDenegada(); 
+            return false;
+        }else{
+            alertaExitosa();
+            localStorage.setItem("usuario", email.value);//ENTREGA2 declaarar mediante setItem la clave usuario y el valor del input email
+            localStorage.setItem('USER', 'true');
+            
+            
+        }
+};
+//linea 18 agregada para que al log exitoso me guarde el utlimo logueado
+
+
+
+
